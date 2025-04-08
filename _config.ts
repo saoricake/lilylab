15e2bdaa0e86557b14d3bd033bca5871bc1cf7da
saori m <trhvmn.abcn@gmail.com>
2025-04-08 02:12:13 -0300

    add rel='external' and target='_blank' to links dynamically

--- _config.ts
+++ _config.ts
@@ -1,9 +1,12 @@
 import lume from "lume/mod.ts"
 import { dateISO, datePretty } from "./_utils.ts"
 
+// define the site's domain
+const SITE_DOMAIN = new URL("https://proflily.games/")
+
 // initialize the site
 const lilylab = lume({
-  location: new URL("https://proflily.games/"),
+  location: SITE_DOMAIN,
   server: { open: true },
 })
 
@@ -14,11 +17,24 @@ lilylab.ignore("README.md")
 lilylab.data("dateISO", dateISO)
 lilylab.data("datePretty", datePretty, "/blog")
 
-// copy the CSS folder to the output folder
+// copy the CSS and blog image folders to the output folder
 lilylab.copy("css")
 lilylab.copy("blog/img")
 
 // keep dates on post URLs
 lilylab.parseBasename(basename => ({ basename }))
+
+// set links to other sites to open in a new tab
+lilylab.process([".html"], (pages: Lume.Page[]) => {
+  for (const page of pages) {
+    if (!page.document) continue;
+    for (const link of page.document.getElementsByTagName("a")) {
+      if (link.origin !== SITE_DOMAIN.origin) {
+        link.setAttribute("rel", "external")
+        link.setAttribute("target", "_blank")
+      }
+    }
+  }
+})
 
 export default lilylab
